1419857f526ced9a6583608e536bebc71a087639
George Gomes <georgegomees@gmail.com>
2018-12-20 14:38:11 -0200

    Fix sequelize route

--- modules/db.js
+++ modules/db.js
@@ -4,7 +4,10 @@ const prodSequelize = require('./dbCredentials');
 var sequelize
 
 if(process.env.NODE_ENV == "production") {
-    sequilize = prodSequelize
+    sequelize = new Sequelize('heroku_343c56030cd59ea','b4defcec75d888', 'f579f2e3', {
+        host: "us-cdbr-iron-east-01.cleardb.net",
+        dialect: 'mysql'
+    });
 } else {
     sequelize = new Sequelize('mulltometro','root', '123123', {
         host: "localhost",
