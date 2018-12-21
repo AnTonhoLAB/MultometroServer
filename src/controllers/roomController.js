f22881b99c116e66751593c940c6e075e1f65233
George Gomes <georgegomees@gmail.com>
2018-12-21 14:25:30 -0200

    Create a controller to manage requests and call methods by router

---
+++ src/controllers/roomController.js
@@ -0,0 +1,53 @@
+const userModel = require('../model/userModel');
+const roomModel = require('../model/roomModel');
+const userInRoom = require('../model/userInRoomModel');
+
+function createRoom(roomToSave, user) {
+
+    return roomModel.create({
+        name: roomToSave.name,
+        dueDate: roomToSave.dueDate,
+        color: roomToSave.color,
+    }).then(savedRoom => {
+        const userSavedRoom = userInRoom.create({
+            userType: user.type,
+            enterDate: new Date(),
+            mulltometroUserId: user.mulltometroUserId,
+            roomId: savedRoom.id
+        });
+        return userSavedRoom;
+    })
+    .then(_userInRoom => {
+        return roomModel.findById( _userInRoom.id, roomInformationFilter);
+    })
+    .catch(err => {
+        return err;
+    });
+}
+
+function getRoomById(room) {
+    return roomModel.findById( room.id, roomInformationFilter)
+        .then( room => {
+            return room
+        })
+        .catch(err => {
+            return err
+        });
+}
+
+const roomInformationFilter = {
+    attributes: ['id', 'name','dueDate', 'color', 'createdAt' ],
+    include: [{    
+            attributes: ['userType', 'enterDate','mulltometroUserId' ],
+            model: userInRoom, 
+            include:[ {
+                    attributes: ['userName', 'email', 'photoURL' ], 
+                    model:   userModel 
+                }]
+        }]
+} 
+
+module.exports = { 
+   createRoom: createRoom,
+   getRoomById: getRoomById
+}
--- src/routes/room.js
+++ src/routes/room.js
@@ -1,63 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
-const userModel = require('../model/userModel');
 const roomModel = require('../model/roomModel');
-const userInRoom = require('../model/userInRoomModel');
+
+const roomController = require('../controllers/roomController');
 
 
 router.post("/create", (req, res) => {
     const roomToSave = req.body.room
     const user  = req.body.user
 
-    roomModel.create({
-        name: roomToSave.name,
-        dueDate: roomToSave.dueDate,
-        color: roomToSave.color,
-    }).then(savedRoom => {
-        const userSavedRoom = userInRoom.create({
-            userType: user.type,
-            enterDate: new Date(),
-            mulltometroUserId: user.mulltometroUserId,
-            roomId: savedRoom.id
+    roomController.createRoom(roomToSave, user)
+        .then( room => {
+            res.status(200).send( { room: room} );
+        })
+        .catch(err =>{
+            res.status(500).send({ message: err });
         });
-
-        return userSavedRoom;
-    })
-    .then(_userInRoom => {
-        return roomModel.findById( _userInRoom.id, roomInformationFilter);
-    })
-    .then( room => {
-        const abc = room.userInRooms[0].mulltometroUser
-        res.status(200).send( { room: room} );
-    })
-    .catch(err => {
-        res.status(500).send({ message: err });
-    });
 });
 
 router.post("/getRoomById", (req, res) => {
     const room = req.body.room
 
-    roomModel.findById( room.id, roomInformationFilter)
+    roomController.getRoomById(room)
         .then( room => {
             res.status(200).send( { room: room} );
         })
         .catch(err => {
             res.status(500).send({ message: err });
         });
-});
 
-const roomInformationFilter = {
-    attributes: ['id', 'name','dueDate', 'color', 'createdAt' ],
-    include: [{    
-            attributes: ['userType', 'enterDate','mulltometroUserId' ],
-            model: userInRoom, 
-            include:[ {
-                    attributes: ['userName', 'email', 'photoURL' ], 
-                    model:   userModel 
-                }]
-        }]
-} 
+    // roomModel.findById( room.id, roomInformationFilter)
+    //     .then( room => {
+    //         res.status(200).send( { room: room} );
+    //     })
+    //     .catch(err => {
+    //         res.status(500).send({ message: err });
+    //     });
+});
 
 module.exports = router;
