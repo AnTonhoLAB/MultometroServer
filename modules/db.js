47650f366ec9978ed7576928f37ed6a720f361cc
George Gomes <georgegomees@gmail.com>
2019-11-01 16:24:32 -0300

    Fix sequelize

--- modules/db.js
+++ modules/db.js
@@ -5,7 +5,10 @@ var sequelize
 if(process.env.NODE_ENV == "production") {
     sequelize = new Sequelize('heroku_343c56030cd59ea','b4defcec75d888', 'f579f2e3', {
         host: "us-cdbr-iron-east-01.cleardb.net",
-        dialect: 'mysql'
+        dialect: 'mysql',
+        logging: false,
+        freezeTableName: true,
+        operatorsAliases: false
     });
 } else {
     sequelize = new Sequelize('mulltometro','root', '123123', {
